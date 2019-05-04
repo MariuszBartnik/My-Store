e7fb2dbd42f96d16cf7ab08f497e03a4edab347a
MariuszBartnik <44741577+MariuszBartnik@users.noreply.github.com>
2019-05-04 07:46:16 +0200

    Update firebaseConfig.js

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,8 +1,8 @@
 export default {
-    apiKey: "AIzaSyDR4DFJ8qtxwz5B6wA5HxqCqZ6HpkAGEzY",
-    authDomain: "mystore-55a6d.firebaseapp.com",
-    databaseURL: "https://mystore-55a6d.firebaseio.com",
-    projectId: "mystore-55a6d",
-    storageBucket: "mystore-55a6d.appspot.com",
-    messagingSenderId: "337598501443"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 }
